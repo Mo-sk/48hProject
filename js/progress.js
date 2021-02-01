1c43b1838f60adec0db5fa1b32adf140c42eef8a
bertyn99 <bboulikou@gmail.com>
2021-02-01 18:41:00 +0100

    ajout du style du form

--- js/progress.js
+++ js/progress.js
@@ -9,7 +9,12 @@ if (!localStorage.getItem(progress)) {
     localStorage.setItem(progress)
 }
 
+document.querySelector('form').submit(e => {
 
 
+    let progress = localStorage.getItem("progress");
+    progress.etape1 = e.submit.value;
+    localStorage
+})
 
 
