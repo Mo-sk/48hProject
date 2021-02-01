7a7b50b7ffe83b44720451198c82368a56420e68
Mo-sk <58175449+Mo-sk@users.noreply.github.com>
2021-02-01 22:45:44 +0100

    html + progress.js

--- js/progress.js
+++ js/progress.js
@@ -5,15 +5,41 @@ let progress = {
     finale: ""
 }
 
-if (!localStorage.getItem(progress)) {
-    localStorage.setItem(progress)
+if (localStorage.progress == "") {
+    localStorage.setItem("progress", JSON.stringify(progress));
+    console.log(localStorage.getItem(progress))
 }
 
-document.querySelector('form-cointainer').addEventListener("submit", e => {
-    console.log(e)
+document.querySelector(`.form-container`).addEventListener("submit", e => {
+    e.value
+    e.preventDefault()
+    console.log(e.target[0].value)
     /*  let progress = localStorage.getItem("progress");
      progress.etape1 = e.submit.value;
      localStorage */
+    if(e.target[0].value === "Pr0jet48hYgroupe2") {
+        let progress = JSON.parse(localStorage.getItem("progress"));
+        
+        progress.etape1 = e.target[0].value;
+        
+        localStorage.progress =  JSON.stringify(progress);
+    }
+
+    if(e.target[0].value === "LeDeuxièmeCodee" && localStorage.progress != "") {
+        let progress = JSON.parse(localStorage.getItem("progress"));
+        
+        progress.etape2 = e.target[0].value;
+        
+        localStorage.progress =  JSON.stringify(progress);
+    }
+
+    if(e.target[0].value === "TroisièmeCodeee" && localStorage.progress.etape2 != "" && localStorage.progress.etape1 != "") {
+        let progress = JSON.parse(localStorage.getItem("progress"));
+        
+        progress.etape2 = e.target[0].value;
+        
+        localStorage.progress =  JSON.stringify(progress);
+    }
 })
 
 
