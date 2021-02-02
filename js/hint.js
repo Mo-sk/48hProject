876e4c411b4022f6e189b8d0766740b45a8fd12c
Cyriod <71000250+Cyriod@users.noreply.github.com>
2021-02-02 10:17:26 +0100

    Update hint.js
    Ajustement après les ajouts de bertyn
    
    Co-Authored-By: bertyn99 <40544784+bertyn99@users.noreply.github.com>

--- js/hint.js
+++ js/hint.js
@@ -4,27 +4,35 @@ en fonction de leur position dans le site. index = position
 
 Plus gros travail = inventif pour les indices */
 
-// ## Bon taff Amélie ##
-//                  {signé Noé}
-
 /* 
 https://www.figma.com/file/2X73YLkGRLAqjiEBlQLcb4/Untitled?node-id=1%3A11 Arborescence
 
 page 1 {
-    étape 1 - Display: none | index 0 -> Je n'apprécie pas être révélé au grand jour;
-    étape 2a - (Biographie) Cliquer sur: JE - SUIS - JULES CESAR | index 1 -> Ne me titillez pas, cela me chatouille;
+    étape 1 - Display: none                                                     | index 0 -> Je n'apprécie pas être révélé au grand jour;
+    étape 2a - (Biographie) Cliquer sur: JE - SUIS - JULES CESAR                | index 1 -> Ne me titillez pas, cela me chatouille;
     étape 3 - 00000001 01000110 10000001 -> 1+128+512+1024+16384+65536 = 85 585 | index 2 -> 83 585 n'est pas dans le bon ordre;
-    étape 3a - binaire qui correspond à 1 = masjucule et 0 = minuscule | index 3 -> Beep-B00p B11p-Boop;
-
-    
+    étape 3a - binaire qui correspond à 1 = masjucule et 0 = minuscule          | index 3 -> Beep-B00p B11p-Boop;
 }
 
 page 2 {
-
+    étape 4 -   | index 4
+    étape 4a -  | index 5
+    étape 4b - Texte malvoyant pas encore trouvé    | index 6
+    étape 4c - Texte malvoyant trouvé -> donner indice sur la clef César | index 7 -> inférieur à 22
 
 }
 
+page 3 {
+    étape 5 -   | index 8
+    étape 5a -  | index 9
+    étape 5b -  | index 10
+    étape 5c - Le QR Code donne ->  | index 11
+}
 */
+if (!localStorage.getItem('nbHint')) {
+    localStorage.setItem('nbHint', 0);
+}
+
 if (!localStorage.getItem('nbHint')) {
     localStorage.setItem('nbHint', 0);
 }
@@ -62,4 +70,8 @@ function giveHint() {
         }
 
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
