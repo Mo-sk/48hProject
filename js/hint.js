0b99d038da29d0ae3a3d41826002bc2a2fa9c071
bertyn99 <bboulikou@gmail.com>
2021-02-02 11:55:00 +0100

    correction condition giveHint

--- js/hint.js
+++ js/hint.js
@@ -33,17 +33,13 @@ if (!localStorage.getItem('nbHint')) {
     localStorage.setItem('nbHint', 0);
 }
 
-if (!localStorage.getItem('nbHint')) {
-    localStorage.setItem('nbHint', 0);
-}
-
 
 let hint = ["Je n'apprécie pas être révélé au grand jour", "Ne me titillez pas, cela me chatouille", "83 585 n'est pas dans le bon ordre", "BEEP-b00p B11P-boop", "J'ai la nausée à cause de vous ! GARDES !", "Un QR Code.. comme c'est original"]
 
 function giveHint() {
 
     if (window.location.href.includes('index')) {
-        if (JSON.parse(localStorage.nbHint) >= 0 || JSON.parse(localStorage.nbHint) < 3) {
+        if (JSON.parse(localStorage.nbHint) >= 0 && JSON.parse(localStorage.nbHint) < 3) {
             console.log(`hint: ${hint[localStorage.nbHint]}`)
         }
         else {
@@ -52,7 +48,7 @@ function giveHint() {
 
     }
     if (window.location.href.includes('hidden')) {
-        if (JSON.parse(localStorage.nbHint) >= 3 || JSON.parse(localStorage.nbHint) < 5) {
+        if (JSON.parse(localStorage.nbHint) >= 3 && JSON.parse(localStorage.nbHint) < 5) {
             console.log(`hint: ${hint[localStorage.nbHint]}`)
         }
         else {
@@ -61,8 +57,8 @@ function giveHint() {
 
 
     }
-    if (window.location.href.includes('hidden')) {
-        if (JSON.parse(localStorage.nbHint) >= 5 || JSON.parse(localStorage.nbHint) < 6) {
+    if (window.location.href.includes('crypt')) {
+        if (JSON.parse(localStorage.nbHint) >= 5 && JSON.parse(localStorage.nbHint) < 6) {
             console.log(`hint: ${hint[localStorage.nbHint]}`)
         }
         else {
@@ -70,4 +66,8 @@ function giveHint() {
         }
 
     }
+    let nbHint = JSON.parse(localStorage.getItem("nbHint"));
+
+    nbHint++;
+    localStorage.nbHint = JSON.stringify(nbHint);
 }
