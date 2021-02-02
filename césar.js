252a8c596d36c2e43e52e2886b7df50d2908b22a
aletrou1 <71063846+cornichonouille@users.noreply.github.com>
2021-02-02 12:44:35 +0100

    2nd essai César code

--- césar.js
+++ césar.js
@@ -1,15 +1,14 @@
-function cesar(str) {
-
-    var solved = "";
-    for (var i=0; i<str.length; i++) {
-        var asciiNum = str[i].charCodeAt();
-        if (asciiNum >= 65 && asciiNum <= 77) {
-            solved += String.fromCharCode(asciiNum + 13);
-        } else if (asciiNum >= 78 && asciiNum <= 90) {
-            solved += String.fromCharCode(asciiNum - 13);
-        } else {
-            solved += str[i];
-        }
+function cesar (string, n){
+    let result = "", code;
+    for (let i=0; i<string.length;i++){
+      if(string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
+        code = (((string.charCodeAt(i)-65)+n)%26)+65;
+      }else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+        code = (((string.charCodeAt(i)-97)+n)%26)+97;
+      }else if (string.charCodeAt(i)===32){
+        code = 32;
+      }
+      result+=String.fromCharCode(code);
     }
-    return solved
-}
+    return result;
+  }
